1b7be8ec9d0b346bba23542416b9247d4de4ce0d
Juan <juanrodriguezcode@gmail.com>
2024-10-08 15:40:07 -0600

    added basic game. error handling and win/loss feedback is still missing

--- main.py
+++ main.py
@@ -9,15 +9,13 @@ def create_matrix(rows,columns):
         
         if ships.count(position) == 0:
             ships.append(position)
-            
-    print(ships)
-    
+                
     count = 0     
                 
     for i in range(rows):
         rows=[]
         for j in range(columns):
-            if ships.count(count) == 1:
+            if count in ships:
                 rows.append(1)
             else:
                 rows.append(0)
@@ -25,21 +23,83 @@ def create_matrix(rows,columns):
             count += 1
         matrix.append(rows)
     
-    
-    print(matrix)
-    
+        
     return matrix
 
-def test_matrix(matrix):
-    counter = 0
-    
-    for rows in matrix:
+def display_rows(board, show_ships):
+    # Then we can iterate through the board's matrix. Since we print by row, we add append all the elements by row in a list, and print the row.
+    # Depending on the value on the board, we display a different element on the board.
+    for row_number, row in enumerate(board):
+        print_row = [str(row_number + 1)]
         
-        for element in rows:
-            if element == 1:
-                counter += 1
-                
-    print(counter)
+        for column_number, element in enumerate(row):  
+            if show_ships:
+                if element == 1 or element == 3:
+                    print_row.append("s")    
+                else:
+                    print_row.append(".")
+
+            else:         
+                if element == 0 or element == 1 and not show_ships:
+                    print_row.append(".")
+                elif element == 2:
+                    print_row.append("x")
+                elif element == 3:
+                    print_row.append("o")    
+                            
+        print(" | ".join(print_row))
 
 
-test_matrix(create_matrix(8,8))
+def display_board(board, show_ships = False):
+    # The first row is the leyend for the column number
+    spacing_string = " " * 3
+    leyend_row = ["#"]
+    
+    
+    # We can get it by iterating on the number of columns and appending the number in the leyend_row 
+    for column_number, column in enumerate(board[0]):
+        leyend_row.append(str(column_number + 1))
+        
+        
+    # And the printing it on the console with a spacing of three spaces
+    print(spacing_string.join(leyend_row))    
+    
+    display_rows(board, show_ships)
+
+
+def main():
+    turn = 0
+    board = create_matrix(8,8)
+    print(board)
+    attempts = []
+    
+
+    while  turn < 10:
+        print(f"TURN {turn + 1}", "\n")
+        display_board(board)
+        
+        attempt_row = int(input("Next row to shoot? (1 - 8) "))
+        attempt_column = int(input("Next column to shoot? (1 - 8) "))
+        
+        if [attempt_row, attempt_column] in attempts:
+            print("You already shot there")
+            continue
+
+        if board[attempt_row - 1][attempt_column - 1] == 0:
+            print("You missed!")
+            board[attempt_row - 1][attempt_column - 1] = 2
+
+        
+        if board[attempt_row - 1][attempt_column - 1] == 1:
+            print("You hit a ship!")
+            board[attempt_row - 1][attempt_column - 1] = 3   
+        
+        attempts.append([attempt_row, attempt_column])
+        
+        
+        turn += 1
+        
+    # Display board showing where are the ships   
+    display_board(board, True)   
+
+main()
